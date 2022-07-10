2321903d22c0a30436b2c3a92904d91434178965
k-i-r-o-o <58162683+k-i-r-o-o@users.noreply.github.com>
2022-07-10 19:27:30 +0530

    initial_Commit

---
+++ server/routes/posts.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+//http://localhost:5000/posts
+router.get('/'  ,(req,res) =>{
+res.send('THIS WORKS!');
+
+});
+
+export default router;
